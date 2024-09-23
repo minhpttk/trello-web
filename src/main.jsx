c386bb48a1c83d82788ac9db02495886c55ea350
minhpttk <dinhminh060102@gmail.com>
2024-09-23 14:10:19 +0700

    update source base

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import CSSBaseline from '@mui/material/CssBaseline'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <CSSBaseline>
+      <App />
+    </CSSBaseline>
   </StrictMode>,
 )
